04bcba9292fef61d9362058542d0c38fe3a03ed6
Steven Xu <stevenx@yorku.ca>
2015-06-02 12:42:38 -0400

    Added nice device log formatting

--- Netdisco/lib/App/NetdiscoX/Web/Plugin/Device/Log.pm
+++ Netdisco/lib/App/NetdiscoX/Web/Plugin/Device/Log.pm
@@ -10,11 +10,12 @@ use App::Netdisco::Web::Plugin;
 use File::Share ':all';
 register_template_path(
   dist_dir( 'App-NetdiscoX-Web-Plugin-Device-Log' ));
+register_css('log');
 
 
 register_device_tab({ tag => 'log', label => 'Log' });
 
-# device details table
+# log table
 ajax '/ajax/content/device/log' => require_login sub {
     my $q = param('q');
     my $device = schema('netdisco')->resultset('Device')
