f9dd4182db063fff4d29d33138c1cddae2cfe423
Steven Xu <stevenx@yorku.ca>
2015-07-23 12:54:04 -0400

    Save cookie in search ports tab

--- Netdisco/share/views/js/common.js
+++ Netdisco/share/views/js/common.js
@@ -78,6 +78,24 @@
       update_browser_history('[% tab.tag %]', pgtitle, '');
       update_csv_download_link('search', '[% tab.tag %]', '[% tab.provides_csv %]');
       do_search(event, '[% tab.tag %]');
+
+      [% IF tab == 'ports' %]
+      // to be fair I can't remember why we do this in JS and not from the app
+      // perhaps because selecting form fields to go in the cookie is easier?
+      var cookie = $('#ports_form').find('input,select')
+        .not('#nd_port-query,input[name="q"],input[name="tab"]')
+        .serializeArray();
+      $('#ports_form').find('input[type="checkbox"]').map(function() {
+        cookie.push({'name': 'columns', 'value': $(this).attr('name')});
+      });
+      $.cookie('nd_ports-form', $.param(cookie) ,{ expires: 365 });
+
+      // form reset icon on ports tab
+      $('#nd_sidebar-reset-link').attr('href', uri_base + '/device?tab=[% tab.tag %]&reset=on&' +
+        $('#ports_form')
+          .find('input[name="q"],input[name="f"],input[name="partial"],input[name="invert"]')
+          .serialize());
+      [% END %]
     });
     [% END %]
     [% END %]
@@ -97,11 +115,11 @@
       var cookie = $('#ports_form').find('input,select')
         .not('#nd_port-query,input[name="q"],input[name="tab"]')
         .serializeArray();
+
       $('#ports_form').find('input[type="checkbox"]').map(function() {
         cookie.push({'name': 'columns', 'value': $(this).attr('name')});
       });
       $.cookie('nd_ports-form', $.param(cookie) ,{ expires: 365 });
-
       // form reset icon on ports tab
       $('#nd_sidebar-reset-link').attr('href', uri_base + '/device?tab=[% tab.tag %]&reset=on&' +
         $('#ports_form')
